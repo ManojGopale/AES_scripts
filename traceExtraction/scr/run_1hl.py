1c066f9f35f21f4bb3cc215050d2414db9661ae0
Manoj Patilbuva Gopale <manojgopale@login2.cm.cluster>
2019-09-25 18:14:14 -0700

    added run_1hl_ver1.0

--- traceExtraction/scr/run_1hl.py
+++ traceExtraction/scr/run_1hl.py
@@ -1,62 +1,46 @@
+## This file will process the matlab data to get back train, dev and test data
 import sys
-sys.path.insert(0, '/extra/manojgopale/AES_data/traceExtraction/scr/')
-import classify_1HL 
+sys.path.insert(0, '/extra/manojgopale/AES_data/traceExtraction/scr/classify_1HL.py')
+import classify_1HL
 
-def runConfig(dataDir, trainDataSize, resultDir, modelName, hiddenSize, batchSize):
-	trainData, devData, testData = classify_1HL.getData(dataDir, trainDataSize)
-	
-	x_train, y_train_oh = trainData
-	x_dev, y_dev_oh = devData
-	x_test, y_test_oh = testData
-	
-	## Instantiate the model and test, dev and training sets
-	classifier = classify_1HL.Classifier(resultDir, modelName, x_train, y_train_oh, x_dev, y_dev_oh, x_test, y_test_oh, hiddenSize)
-	
-	## Train the model
-	classifier.train(batchSize)
-	
-	## Evaluate
-	classifier.evaluate()
-	
-	##Save the model
-	classifier.saveModel()
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('-c','--config',
+									action = 'store', type='string', dest='config', default = 'config3p1')
+parser.add_option('-b', '--batchCount',
+									action = 'store', type='int', dest='batchCount', default = 128)
+parser.add_option('-h', '--hiddenSize',
+									action = 'store', type='int', dest='hiddenSize', default = 1000)
+parser.add_option('--modelName',
+									action = 'store', type='string', dest='modelName', default = 'defaultName')
+
+(options, args) = parser.parse_args()
 
 
-dataDir = "/xdisk/manojgopale/data_csv/config5p4/"
-resultDir = "/extra/manojgopale/AES_data/config5p4_15ktraining/result_new"
-trainDataSize = 15000
+## Collect data
+trainDataSize = 4000 ## There are 4000 traces per key
+config = options.config
+modelName = options.modelName
+hiddenSize = options.hiddenSize
+resultDir = "/xdisk/manojgopale/AES/dataCollection/processedData/run_1_per_key/result/" + config + "/"
 
-#M## 100 hidden neurons
-#MmodelName = "model_1hl_100neurons_1"
-#MhiddenSize = 100
-#MbatchSize = 2048
-#M
-#MrunConfig(dataDir, trainDataSize, resultDir, modelName, hiddenSize, batchSize)
-#M
-#M## 500 hidden neurons
-#MmodelName = "model_1hl_500neurons_2"
-#MhiddenSize = 500
-#MbatchSize = 2048
-#M
-#MrunConfig(dataDir, trainDataSize, resultDir, modelName, hiddenSize, batchSize)
+trainData, devData, testData = classify_1HL.getData(config, trainDataSize)
 
-## 1000 hidden neurons
-modelName = "model_1hl_1000neurons_3"
-hiddenSize = 1000
-batchSize = 2048
+x_train, y_train = trainData
+x_dev, y_dev = devData
+x_test, y_test = testData
 
-runConfig(dataDir, trainDataSize, resultDir, modelName, hiddenSize, batchSize)
+## Instantiate the model and test, dev and training sets
+classifier = classify_1HL.Classifier(resultDir, modelName, x_train, y_train, x_dev, y_dev, x_test, y_test, hiddenSize)
 
-## 21500 hidden neurons
-modelName = "model_1hl_1500neurons_4"
-hiddenSize = 1500
-batchSize = 2048
+## Train the model
+classifier.train(batchSize)
 
-runConfig(dataDir, trainDataSize, resultDir, modelName, hiddenSize, batchSize)
+## Evaluate
+classifier.evaluate()
 
-## 2000 hidden neurons
-modelName = "model_1hl_2000neurons_5"
-hiddenSize = 2000
-batchSize = 2048
+##Save the model
+classifier.saveModel()
 
-runConfig(dataDir, trainDataSize, resultDir, modelName, hiddenSize, batchSize)
+
